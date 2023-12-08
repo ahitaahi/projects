f8ddf78cdb95b5177dc94a17dee56d87df5e8eda
ahitaahi <138967795+ahitaahi@users.noreply.github.com>
2023-12-08 21:54:43 +0530

    Add files via upload

---
+++ graphics.py
@@ -0,0 +1,11 @@
+
+    # code to execute
+#for name in ['ahi','joe','aspa','amy','lily',]:
+    #print("hi",name,"please come to my party")
+    #for i in range (50):
+        #print("Harvard university")
+
+#for name in ['ahi','joe','aspa','amy','lily',]:
+    #print("hi",name,"please come to my party")
+    for i in range (50):
+        print("Harvard university")
